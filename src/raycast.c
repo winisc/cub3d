da529017e5f381f4201c1a2e74c4d259f647e0ba
maya-taki <g.takiyoshi@gmail.com>
2026-07-27 23:10:38 -0300

    TEST:  add features to touch function in raycasting

--- src/raycast.c
+++ src/raycast.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycast.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wini <wini@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mtakiyos <mtakiyos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 11:20:00 by wini              #+#    #+#             */
-/*   Updated: 2026/06/22 12:15:22 by wini             ###   ########.fr       */
+/*   Updated: 2026/07/27 23:08:36 by mtakiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,17 @@ int	touch(float px, float py, t_game *game)
 {
 	int	x;
 	int	y;
+	// int	row_len;
 
-	x = px / BLOCK;
-	y = py / BLOCK;
+	// if (!game || !game->map || px < 0 || py < 0)
+	// 	return (1);
+	// x = (int)(px / BLOCK);
+	// y = (int)(py / BLOCK);
+	// if (!game->map[y])
+	// 	return (1);
+	// row_len = ft_strlen(game->map[y]);
+	// if (x < 0 || x >= row_len)
+	// 	return (1);
 	if (game->map[y][x] == '1')
 		return (1);
 	return (0);
