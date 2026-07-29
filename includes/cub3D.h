b3b74b316abda0a610ceca73959d0a0fb8c8dbef
maya-taki <g.takiyoshi@gmail.com>
2026-07-29 11:34:59 -0300

    Revert "fix: rename size_line to line_len for clarity"
    This reverts commit 8206fb0b70654e7f88c66e35ac4db512ab85bc85.

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -70,7 +70,7 @@ typedef struct s_game
 	void		*img;
 	char		*data;
 	int			bpp;
-	int			line_len;
+	int			size_line;
 	int			endian;
 	t_player	player;
 	char		**map;
--- src/draw.c
+++ src/draw.c
@@ -18,7 +18,7 @@ void	put_pixel(int x, int y, int color, t_game *game)
 
 	if (x >= WIDTH || y >= HEIGHT || x < 0 || y < 0)
 		return ;
-	index = y * game->line_len + x * game->bpp / 8;
+	index = y * game->size_line + x * game->bpp / 8;
 	game->data[index] = color & 0xFF;
 	game->data[index + 1] = (color >> 8) & 0xFF;
 	game->data[index + 2] = (color >> 16) & 0xFF;
@@ -55,5 +55,5 @@ void	draw_wall(t_game *game, int column, float height)
 
 void	clear_image(t_game *game)
 {
-	ft_bzero(game->data, game->line_len * HEIGHT);
+	ft_bzero(game->data, game->size_line * HEIGHT);
 }
